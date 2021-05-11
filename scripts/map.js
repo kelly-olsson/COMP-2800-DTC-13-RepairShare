279c5f2f37ea10a9d127615aeedcdfa3dfb31eb7
Hannah Kuo <hannahkuo0205@gmail.com>
2021-05-10 18:10:12 -0700

    Implement functions to read from firestore in map.js

--- scripts/map.js
+++ scripts/map.js
@@ -53,11 +53,37 @@ function initMap() {
 }
 
 
+// Add provider marker, read data from Firestore 
+function readCoordinateFromDatabase() {
+    firebase.auth().onAuthStateChanged(function (somebody) {
+        if (somebody) {
+            db.collection("users") 
+                .doc(somebody.uid) 
+                .get() 
+                .then(function (doc) {
+                    var n = doc.data().Location; 
+                    $("#location-goes-here").text(n); 
+                })
+        }})
+}
+readCoordinateFromDatabase();
 
 
-
-
-
+// Read user info from firebase and greet user based on user's name.
+function sayHello() {
+    firebase.auth().onAuthStateChanged(function (somebody) {
+        if (somebody) {
+            db.collection("users") 
+                .doc(somebody.uid) 
+                .get() 
+                .then(function (doc) {
+                    var n = doc.data().name; 
+                    $("#name-goes-here").text(n); 
+                })
+        }
+    })
+}
+sayHello();
 
 
 
