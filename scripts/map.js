05832298ea9fb311a4578a3afb1d3a293ba50fbf
Mahan Esmaeilzadeh <mesmaeilzadeh@my.bcit.ca>
2021-05-13 19:22:31 -0700

    Added a closure function to add markers. PP:Hannah

--- scripts/map.js
+++ scripts/map.js
@@ -1,10 +1,10 @@
 // CITATION FOR GEOLOCATION: https://www.aspsnippets.com/Articles/Show-users-current-location-on-Google-Map-using-GeoLocation-API-in-website.aspx
-var locationArray;
+// var locationArray;
 var megaArray;
 var newMEGAArray;
 var map;
 // https://developers.google.com/maps/documentation/javascript/overview?hl=en_US#maps_map_simple-javascript
-function initMap(doc) {
+function initMap() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (p) {
             var LatLng = new google.maps.LatLng(p.coords.latitude, p.coords.longitude);
@@ -15,66 +15,8 @@ function initMap(doc) {
             map = new google.maps.Map(document.getElementById("map"), mapOptions);
         })
     }
-
-
-    // // The marker, positioned at location of the user who has logged in 
-    // const marker = new google.maps.Marker({
-    //     position: location,
-    //     map: map,
-    //     // icon: "repair.png"
-    // });
-
-
-    //this is how you add a window with some content to the map
-    // const detailWindow = new google.maps.InfoWindow({
-    //     content: "<h3> Name: <span id='window-name'>User</span> </h3> <h3> Rating: <span id='window-rating'></span> </h3> <h3> Description: <span id='window-description'></span> </h3> <h3> Tools: <span id='window-tools'></span> </h3>"
-    // });
-
-
-
-    // //this pops open the content that was set 
-    // marker.addListener("click", () => {
-    //     detailWindow.open(map, marker);
-    //     mapDetailWindow();
-
-    // })
 }
 
-
-
-
-
-// Pull data from firestore to pass to initMap function
-db.collection("users").get().then((snapshot) => {
-    snapshot.docs.forEach(function (doc) {
-        // console.log(doc.data().location);
-        initMap(doc);
-        // searchForTools(doc);
-    })
-})
-
-// SOURCES: https://stackoverflow.com/questions/11106671/google-maps-api-multiple-markers-with-infowindows
-// http://jsfiddle.net/2crQ7/
-
-// var content = "Loan Number: " + loan +  '</h3>' + "Address: " + add     
-
-// var infowindow = new google.maps.InfoWindow()
-
-// google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ 
-//       return function() {
-//          infowindow.setContent(content);
-//          infowindow.open(map,marker);
-//       };
-//   })(marker,content,infowindow)); 
-
-  // END OF THEFT
-  
-  
-
-
-
-
-// function to listen to the submit event and call searchForTools
 function addSubmitListener() {
     document.getElementById("submit").addEventListener("click", function () {
         var toolKeyword = document.getElementById("tool-keyword").value;
@@ -82,62 +24,6 @@ function addSubmitListener() {
     })
 }
 
-// function to search for tools with the keyword entered from borrow.html
-// function searchForTools(toolKeyword) {
-//     firebase.auth().onAuthStateChanged(function (user) {
-//         db.collection("users")
-//             .doc(user.uid)
-//             .collection("Tools")
-//             .where(toolKeyword, "==", true)
-//             .get()
-//             .then(function (snapshot) {
-//                 snapshot.forEach(function (doc) {
-//                     console.log("keyword search works");
-//                     var toolsObject = doc.data();
-//                     test = []
-//                     for (var key in toolsObject) {
-//                         if (toolsObject[key] == true) {
-//                             getLocation(user.uid);
-//                             test.push(toolsObject);
-//                         }
-//                     }
-//                 })
-//                 return test;
-//             })
-//     })
-// }
-
-
-
-function getLocation(param) {
-    db.ref("users/")
-        .get()
-        .then(function (doc) {
-            console.log(doc.data().location)
-        })
-}
-
-
-
-
-
-// Add provider marker, read data from Firestore 
-// function readCoordinateFromDatabase() {
-//     firebase.auth().onAuthStateChanged(function (somebody) {
-//         if (somebody) {
-//             db.collection("users")
-//                 .doc(somebody.uid)
-//                 .get()
-//                 .then(function (doc) {
-//                     var n = doc.data().location;
-//                     $("#location-goes-here").text(n);
-//                 })
-//         }
-//     })
-// }
-
-
-// Read user info from firebase and greet user based on user's name.
 function sayHello() {
     firebase.auth().onAuthStateChanged(function (somebody) {
         if (somebody) {
@@ -152,41 +38,24 @@ function sayHello() {
     })
 }
 
-// function hannahSearch(toolKeyword) {
-//     db.collection("users").doc(uid)
-//         .collection("tools").doc
-//         .where(toolKeyword, "==", true)
-//         .get()
-//         .then(function (snapshot) {
-//             snapshot.forEach(function (doc) {
-//                 console.log("hannah search works");
-//                 toolsObject = doc.data().tools;
-//                 console.log(Object.keys(toolsObject));
-//                 console.log(Object.values(toolsObject));
-//             })
-//         })
-// }
-
-
-
 // [[GOOLEOBJECTMARKER , UID], [GOOLEOBJECTMARKER , UID]]
-function addInfoWindow(newMEGAArray){
-    for (let i = 0; i < megaArray.length; i++) {
-        var detailWindow = new google.maps.InfoWindow({
-            content: "<h3> Name: <span id='window-name'>User</span> </h3> <h3> Rating: <span id='window-rating'></span> </h3> <h3> Description: <span id='window-description'></span> </h3> <h3> Tools: <span id='window-tools'></span> </h3>"
-        });
-            var marker = newMEGAArray[i][0]
-            var userID = newMEGAArray[i][1]
-            //this pops open the content that was set 
-            marker.addListener("click", () => {
-                detailWindow.open(map, marker);
-                console.log("Issue is map detail window");
-                console.log(userID);
-                mapDetailWindow(userID);
+// function addInfoWindow(newMEGAArray){
+//     for (let i = 0; i < megaArray.length; i++) {
+//         var detailWindow = new google.maps.InfoWindow({
+//             content: "<h3> Name: <span id='window-name'>User</span> </h3> <h3> Rating: <span id='window-rating'></span> </h3> <h3> Description: <span id='window-description'></span> </h3> <h3> Tools: <span id='window-tools'></span> </h3>"
+//         });
+//             var marker = newMEGAArray[i][0]
+//             var userID = newMEGAArray[i][1]
+//             //this pops open the content that was set 
+//             marker.addListener("click", () => {
+//                 detailWindow.open(map, marker);
+//                 console.log("Issue is map detail window");
+//                 console.log(userID);
+//                 mapDetailWindow(userID);
         
-            })
-}
-}
+//             })
+// }
+// }
 
 function mapDetailWindow(userID) {
     // console.log(userID);
@@ -204,11 +73,44 @@ function mapDetailWindow(userID) {
         })
 }
 
-// [
-//  [[long:, lat:] , UID], 
-//  [[long:, lat:] , UID]
-// ]
+function setMarkers(map, megaArray){
+  
+    var marker, i
 
+for (i = 0; i < megaArray.length; i++){  
+
+    var lat = megaArray[i][0][0]
+    var lng = megaArray[i][0][1]
+    var userID = megaArray[i][1]
+    console.log(lng)
+    console.log(lat)
+    console.log(typeof lat)
+
+
+    var location = new google.maps.LatLng(lat, lng);
+
+var marker = new google.maps.Marker({  
+        map: map, 
+        position: location  
+      });
+
+
+var content = "<h3> Name: <span id='window-name'>User</span> </h3> <h3> Rating: <span id='window-rating'></span> </h3> <h3> Description: <span id='window-description'></span> </h3> <h3> Tools: <span id='window-tools'></span> </h3>"
+mapDetailWindow(userID)
+var infowindow = new google.maps.InfoWindow()
+
+google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ 
+      return function() {
+         infowindow.setContent(content);
+         infowindow.open(map,marker);
+      };
+  })(marker,content,infowindow)); 
+
+};
+
+}
+
+///A COPY 
 function getLocationH(toolKeyword) {
     db.collection("users")
         .where("tools." + toolKeyword, '==', true)
@@ -217,7 +119,7 @@ function getLocationH(toolKeyword) {
             megaArray = []   // [[[long:, lat:] , UID], [[long:, lat:] , UID]]
             snapshot.forEach(function (doc) {
                 var tempArray = []
-
+                
                 tempArray.push(doc.data().location)
                 tempArray.push(doc.id)
                 console.log("user uid line 223")
@@ -225,53 +127,14 @@ function getLocationH(toolKeyword) {
 
                 megaArray.push(tempArray)
             })
-            console.log("megaArray line 229")
-            console.log(megaArray)
+            // console.log("megaArray line 229")
+            // console.log(megaArray)
             return megaArray
         })
-        .then(addMarkerToMap(megaArray))
+        .then(setMarkers(map, megaArray))
 }
 
-// megaArray = [[[long:, lat:] , UID], [[long:, lat:] , UID]]
-
-// return this -> [[GOOLEOBJECTMARKER , UID], [GOOLEOBJECTMARKER , UID]]
-
-//a function to add multiple markers to the map based on secified coordinates
-function addMarkerToMap(megaArray) {
-    newMEGAArray = []
-    for (let i = 0; i < megaArray.length; i++) {
-        var newTEMPArray = []
-        let accessedUID = megaArray[i][1]
-        let newLocation = new Map()
-        newLocation['lat'] = megaArray[i][0][0]
-        newLocation['lng'] = megaArray[i][0][1]
-        var location = newLocation;
-        console.log(location)
-        var newmarker = new google.maps.Marker({
-            position: location,
-            zoom: 11,
-            map: map,
-        });
-        newTEMPArray.push(newmarker)
-        newTEMPArray.push(accessedUID)
-        // markers.push[newmarker]
-        // console.log(markers)  
-        newMEGAArray.push(newTEMPArray)
-        console.log("newTEMPArray")
-        console.log(newTEMPArray)
-    }
-    console.log(newMEGAArray)
-    addInfoWindow(newMEGAArray)
-
-};
-
-
-
-
-
-
-
-
-sayHello();
+initMap();
+// sayHello();
 addSubmitListener();
 // readCoordinateFromDatabase();
--- scripts/scratch-map.js
+++ scripts/scratch-map.js
@@ -53,12 +53,12 @@ function initMap(doc) {
 // })
 // }
 
-    // // The marker, positioned at location of the user who has logged in 
-    // const marker = new google.maps.Marker({
-    //     position: location,
-    //     map: map,
-    //     // icon: "repair.png"
-    // });
+    // The marker, positioned at location of the user who has logged in 
+    const marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        // icon: "repair.png"
+    });
 
 
     //this is how you add a window with some content to the map
@@ -557,3 +557,75 @@ function addMarkerToMap(megaArray) {
 sayHello();
 addSubmitListener();
 readCoordinateFromDatabase();
+
+///CLOSURE 
+
+var locations = [
+    [33.890542, 151.274856,],
+    ['loan 2', 33.923036, 151.259052, 'address 2'],
+    ['loan 3', 34.028249, 151.157507, 'address 3'],
+    ['loan 4', 33.80010128657071, 151.28747820854187, 'address 4'],
+    ['loan 5', 33.950198, 151.259302, 'address 5']
+    ];
+  
+    function initialize() {
+  
+      var myOptions = {
+        center: new google.maps.LatLng(33.890542, 151.274856),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+  
+      };
+      var map = new google.maps.Map(document.getElementById("default"),
+          myOptions);
+  
+      setMarkers(map,locations)
+  
+    }
+  
+  
+  
+    function setMarkers(map,locations){
+  
+        var marker, i
+  
+  for (i = 0; i < locations.length; i++)
+   {  
+  
+   var lat = locations[i][1]
+   var long = locations[i][2]
+  
+   latlngset = new google.maps.LatLng(lat, long);
+  
+    var marker = new google.maps.Marker({  
+            map: map, 
+            position: latlngset  
+          });
+  
+  
+          var content = "Loan Number: " + loan +  '</h3>' + "Address: " + add     
+  
+    var infowindow = new google.maps.InfoWindow()
+  
+  google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ 
+          return function() {
+             infowindow.setContent(content);
+             infowindow.open(map,marker);
+          };
+      })(marker,content,infowindow)); 
+  
+    }
+    }
+
+    ///ANOTHER ONE
+
+
+    var infowindow = new google.maps.InfoWindow();
+    google.maps.event.addListener(marker, 'mouseover', (function(marker) {
+               return function() {
+                   var content = address;
+                   infowindow.setContent(content);
+                   infowindow.open(map, marker);
+               }
+             })(marker));
+  
