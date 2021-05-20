9ce9339ff6b7db8ca8a9d0e50aa23a8e987e5e89
Hannah Kuo <hannahkuo0205@gmail.com>
2021-05-20 15:31:49 -0700

    Cleaned up uploadUserProfilePic function in form.js

--- scripts/form.js
+++ scripts/form.js
@@ -97,34 +97,20 @@ function resetForm() {
 }
 
 
-//IMAGE STUFF FROM CARLY#########################################################
-// const user = "sC1n34ukb7RFTJCBi7zsjuqKkyr1"
-
+// Upload user profile image to Cloud Firestore.
 function uploadUserProfilePic(userUid) {
+
     // Let's assume my storage is only enabled for authenticated users 
     // This is set in your firebase console storage "rules" tab
     if (!userUid) { console.err("Not logged in!"); return };
 
-    console.log("uploadUserProfilePic is being called")
-
-    // firebase.auth().onAuthStateChanged(function (user) {
-        const fileInput = document.getElementById("profile-pic");   // pointer #1
-        // const image = document.getElementById("mypic-goes-here"); // pointer #2
-
-        console.log("line 113")
+        const fileInput = document.getElementById("profile-pic");   
 
         // listen for file selection
         fileInput.addEventListener('change', function (e) {
-            
-            // test to ensure correct data collection
-            // db.collection("users").doc(user.uid)
-            // .update({
-            //     "set": "set3"
-            // })
+
             var file = e.target.files[0];
-            var blob = URL.createObjectURL(file);
-            // image.src = blob;            // display this image
-            console.log("We are inside the change eventlistener")
+
             //store using this name
             var storageRef = storage.ref("images/" + userUid + ".jpg"); 
             
@@ -134,7 +120,7 @@ function uploadUserProfilePic(userUid) {
                     console.log('Uploaded to Cloud Storage.');
                 })
 
-						//get the URL of stored file
+			//get the URL of stored file
             storageRef.getDownloadURL()
                 .then(function (url) {   // Get URL of the uploaded file
                     console.log(url);    // Save the URL into users collection
@@ -146,9 +132,7 @@ function uploadUserProfilePic(userUid) {
                     })
                 })
         })
-    
 }
-// uploadUserProfilePic();
 
 // Load user's data
 firebase.auth().onAuthStateChanged(user => {
