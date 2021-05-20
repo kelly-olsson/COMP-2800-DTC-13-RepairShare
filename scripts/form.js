56cd4c577cf0a5fa6f0c7a159a8140e5c9ff0c0f
Hannah Kuo <hannahkuo0205@gmail.com>
2021-05-20 16:07:01 -0700

    Add geolocation getCurrentPosition functionality to form.js, and write to Firestore

--- scripts/form.js
+++ scripts/form.js
@@ -143,3 +143,46 @@ firebase.auth().onAuthStateChanged(user => {
         });
     }
 });
+
+
+// GOECODING STUFF
+// https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
+
+
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  function success(pos) {
+    var crd = pos.coords;
+  
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            userId = user.uid;
+            db.collection("users").doc(user.uid)
+            .update({
+                "latitude": crd.latitude,
+                "longitude": crd.longitude
+            })
+        }
+    });
+
+    // return crd.latitude, crd.longitude
+  }
+  
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
+
+//   let latitude, longitude = navigator.geolocation.getCurrentPosition(success, error, options);
+//   console.log(latitude)
+//   console.log(longitude)
