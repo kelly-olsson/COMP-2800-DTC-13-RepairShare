b3b5957bca1232f5581434f5678ba778b59c68cc
Hannah Kuo <hannahkuo0205@gmail.com>
2021-05-20 14:47:31 -0700

    Add upload picture functionality to form. PP: Mahan.

--- scripts/form.js
+++ scripts/form.js
@@ -7,7 +7,8 @@ function addSubmitListener() {
         let toolsObject = createToolsObject();
         // console.log(toolsObject)
         addData(name, skillsArray, toolsObject);
-        resetForm();
+        // uploadUserProfilePic();
+        // resetForm();
     })
 }
 addSubmitListener();
@@ -18,7 +19,7 @@ function resetFormButton() {
         resetForm();
     })
 }
-resetFormButton();
+// resetFormButton();
 
 
 // Create an array of skills entered by user.
@@ -73,40 +74,6 @@ function addData(name, skillsArray, toolsObject) {
     })
 }
 
-// Create an array in Firestore for each day in the week, containing the habits that are to be done on that day.
-// For example, the "Monday" array contains habits that are to be performed on Mondays
-function updateSkillsArray(uid, name, mon, tue, wed, thurs, fri, sat, sun) {
-    var obj = {};
-    if (mon) {
-        obj.monday = firebase.firestore.FieldValue.arrayUnion(name);
-    }
-    if (tue) {
-        obj.tuesday = firebase.firestore.FieldValue.arrayUnion(name);
-    }
-    if (wed) {
-        obj.wednesday = firebase.firestore.FieldValue.arrayUnion(name);
-    }
-    if (thurs) {
-        obj.thursday = firebase.firestore.FieldValue.arrayUnion(name);
-    }
-    if (fri) {
-        obj.friday = firebase.firestore.FieldValue.arrayUnion(name);
-    }
-    if (sat) {
-        obj.saturday = firebase.firestore.FieldValue.arrayUnion(name);
-    }
-    if (sun) {
-        obj.sunday = firebase.firestore.FieldValue.arrayUnion(name);
-    }
-    db.collection("users").doc(uid)
-        .set(obj, {
-            merge: true
-        })
-        .then(function () {
-            window.location.href = "calendar.html";
-        })
-}
-
 
 function sayHello() {
     firebase.auth().onAuthStateChanged(function (somebody) {
@@ -128,3 +95,82 @@ sayHello();
 function resetForm() {
     document.getElementById("user-form").reset();
 }
+
+
+//IMAGE STUFF FROM CARLY#########################################################
+// const user = "sC1n34ukb7RFTJCBi7zsjuqKkyr1"
+
+function uploadUserProfilePic(userUid) {
+    // Let's assume my storage is only enabled for authenticated users 
+    // This is set in your firebase console storage "rules" tab
+    if (!userUid) { console.err("Not logged in!"); return };
+
+    console.log("uploadUserProfilePic is being called")
+
+    // firebase.auth().onAuthStateChanged(function (user) {
+        const fileInput = document.getElementById("profile-pic");   // pointer #1
+        const image = document.getElementById("mypic-goes-here"); // pointer #2
+
+        console.log("line 113")
+
+        // listen for file selection
+        fileInput.addEventListener('change', function (e) {
+            
+            // test to ensure correct data collection
+            // db.collection("users").doc(user.uid)
+            // .update({
+            //     "set": "set3"
+            // })
+            var file = e.target.files[0];
+            var blob = URL.createObjectURL(file);
+            image.src = blob;            // display this image
+            console.log("We are inside the change eventlistener")
+            //store using this name
+            var storageRef = storage.ref("images/" + userUid + ".jpg"); 
+            
+            //upload the picked file
+            storageRef.put(file) 
+                .then(function(){
+                    console.log('Uploaded to Cloud Storage.');
+                })
+
+						//get the URL of stored file
+            storageRef.getDownloadURL()
+                .then(function (url) {   // Get URL of the uploaded file
+                    console.log(url);    // Save the URL into users collection
+                    db.collection("users").doc(userUid).update({
+                        "profilePic": url
+                    })
+                    .then(function(){
+                        console.log('Added Profile Pic URL to Firestore.');
+                    })
+                })
+        })
+    
+}
+// uploadUserProfilePic();
+
+// Load user's data
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        userId = user.uid;
+        db.collection("users").doc(user.uid).get().then(function(user) {
+            // let userData = user.data();
+            uploadUserProfilePic(userId);
+            // loadUserProfile(userData);
+        });
+    }
+});
+
+
+
+
+function showUploadedPicture(){
+    const fileInput = document.getElementById("profile-pic");   // pointer #1
+    const image = document.getElementById("mypic-goes-here");   // pointer #2
+    fileInput.addEventListener('change', function(e){        //event listener
+        var blob = URL.createObjectURL(e.target.files[0]);
+        image.src = blob;    //change DOM image
+    })
+}
+// showUploadedPicture();
