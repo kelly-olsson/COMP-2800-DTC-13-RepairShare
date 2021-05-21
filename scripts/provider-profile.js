d12b3f88e6782a2f7b93e2ed89e7f1af0e62c26f
Max Anderson-Baier <77411077+MaxAndersonBaier@users.noreply.github.com>
2021-05-20 17:11:35 -0700

    hitting my head against a wall

--- scripts/provider-profile.js
+++ scripts/provider-profile.js
@@ -1,7 +1,8 @@
-const params = new URLSearchParams(window.location.search); 
+const params = new URLSearchParams(window.location.search);
 
-let user_identification = params.get("id");
-console.log(user_identification);
+
+let provider_identification = params.get("id");
+console.log(provider_identification);
 
 function PopulateProviderProfile(userID) {
     db.collection("users")
@@ -13,7 +14,7 @@ function PopulateProviderProfile(userID) {
             var skills = doc.data().skills;
             var tools = doc.data().tools;
             var picture = doc.data().profilePicture;
-            var userattributes = doc.data().attribute; 
+            var userattributes = doc.data().attribute;
 
 
             $(".usernamegoeshere").text(name.toUpperCase());
@@ -22,45 +23,45 @@ function PopulateProviderProfile(userID) {
             $("#profile-photo").attr("src", picture);
 
 
-            for(var index = 0; index < skills.length; index++){          
-                let skill = skills[index];              
+            for (var index = 0; index < skills.length; index++) {
+                let skill = skills[index];
 
-                $(document).ready(function(){
+                $(document).ready(function () {
                     var $skillset = '<li>' + skill + '</li>';
                     $("#skillsinfo").append($skillset)
                 })
             }
 
-            Object.keys(tools).forEach(key=>{
+            Object.keys(tools).forEach(key => {
                 if (tools[key] == true) {
-                  let toolitem = key; 
+                    let toolitem = key;
 
-                  $(document).ready(function(){
-                      var $toolkit = '<li>'+ toolitem + '</li>'
-                      $("#toolz").append($toolkit);
-                  })
+                    $(document).ready(function () {
+                        var $toolkit = '<li>' + toolitem + '</li>'
+                        $("#toolz").append($toolkit);
+                    })
 
-              }
-                
+                }
+
             })
 
-            Object.keys(userattributes).forEach(key=>{
-                if (userattributes[key] == true && key == 'Handy Certified'){
+            Object.keys(userattributes).forEach(key => {
+                if (userattributes[key] == true && key == 'Handy Certified') {
 
                     let handy = key;
                     let handyicon = '<span class="iconify" id="tooldescription" data-icon="bpmn:hand-tool" data-inline="false" data-height="32"></span> '
 
 
-                    $(document).ready(function(){ 
+                    $(document).ready(function () {
                         var $handy = '<p id= "description">' + handyicon + handy + '</span>' + '</p>'
                         $('#userattributes').append($handy);
                     })
                 }
-                else if (userattributes[key] == true && key == 'Tool Share Member'){
+                else if (userattributes[key] == true && key == 'Tool Share Member') {
 
-                    let tooly = key; 
+                    let tooly = key;
                     let toolicon = '<span class="iconify" id="tooldescription" data-icon="ion:hammer-sharp" data-inline="false" data-height="32"></span> ';
-                    $(document).ready(function(){ 
+                    $(document).ready(function () {
                         var $handy = '<p id= "description">' + toolicon + tooly + '</span>' + '</p>'
                         $('#userattributes').append($handy);
                     })
@@ -68,48 +69,48 @@ function PopulateProviderProfile(userID) {
             })
 
 
-        }).catch(function(error){
+        }).catch(function (error) {
             console.log(error)
         })
 }
 
 
-function chatButton(providerID){
-    $('#chatboxbutton').attr("href", "chat.html?id=" + user_identification);
-}
-
-
-// function LeaveReview(providerID, userID){
-//     var review = document.getElementById
-
-
+// function chatButton(providerID) {
+//     $('#chatboxbutton').attr("href", "chat.html?id=" + provider_identification);
 // }
 
-PopulateProviderProfile(user_identification);
-chatButton(user_identification);
+
+// function LeaveReview(providerID, review) {
+//     db.collection("users").doc(providerID)
+//         .update({
+//             "reviews": review
+//         })
+//         , { merge: true }
+// }
+
+
+// let formtext = document.getElementById("exampleFormControlTextarea1");
+// let reviewButton = document.getElementById("reviewsubmit");
+
+// function submitReview(provider_identification, formtext) {
+//     let reviewButton = document.getElementById("reviewsubmit");
+//     reviewButton.addEventListener("click", LeaveReview(provider_identification, formtext));
+// }
 
 
 
+// function addSubmitListener() {
+//     let toolKeyword = document.getElementById("tool-keyword").value;
+//     removeMarkers(markersList);
+//     getLocationH(toolKeyword);
+// }
 
-function mapDetailWindow(userID, i) {
-    db.collection("users")
-        .doc(userID)
-        .get()
-        .then(function (doc) {
-            let name = doc.data().name;
-            let rating = doc.data().rating;
-            let description = doc.data().description;
-            let picture = doc.data().profilePicture;
-            $('#window-name_' + i + '').attr("href", "provider-profile.html?id=" + userID).text(name);
-            $('#window-rating_' + i + '').text(rating);
-            $('#window-description_' + i + '').text(description);
-            $('.profile-pic_' + i + '').attr("src", picture);
+// submitButton.onclick = addSubmitListener;
 
-        }).catch(function(error){
-            console.log(error)
-        })
-}
 
+PopulateProviderProfile(provider_identification);
+// reviewButton.onclick = submitReview(provider_identification, formtext);
+chatButton(provider_identification);
 
 
 
@@ -128,7 +129,7 @@ function mapDetailWindow(userID, i) {
 //                 "skills": skillsArray,
 //                 "tools": toolsObject
 
-//             }, {merge: true})
+//             }, { merge: true })
 //             // .then(function () {
 //             //     updateSkillsArray(user.uid, name, mon, tue, wed, thurs, fri, sat, sun);
 //             // })
