950ce29000af7e380e6ca571bfb20194f78f5a2d
Max Anderson-Baier <77411077+MaxAndersonBaier@users.noreply.github.com>
2021-05-23 22:42:16 -0700

    add image for homepage

--- scripts/user-profile.js
+++ scripts/user-profile.js
@@ -11,7 +11,7 @@ function sayHello() {
                     var skills = doc.data().skills;
                     var tools = doc.data().tools;
                     var picture = doc.data().profilePicture;
-                    var userattributes = doc.data().attribute; 
+                    var userattributes = doc.data().attribute;
 
 
                     $("#usernamegoeshere").text(name.toUpperCase());
@@ -19,26 +19,26 @@ function sayHello() {
                     $("#about").text(description);
                     $("#profile-photo").attr("src", picture);
 
-                    for(var index = 0; index < skills.length; index++){          
-                        let skill = skills[index];              
+                    for (var index = 0; index < skills.length; index++) {
+                        let skill = skills[index];
 
-                        $(document).ready(function(){
+                        $(document).ready(function () {
                             var $skillset = '<li>' + skill + '</li>';
                             $("#skillsinfo").append($skillset)
                         })
                     }
 
-                    Object.keys(tools).forEach(key=>{
+                    Object.keys(tools).forEach(key => {
                         if (tools[key] == true) {
-                          let toolitem = key; 
+                            let toolitem = key;
 
-                          $(document).ready(function(){
-                              var $toolkit = '<li>'+ toolitem + '</li>'
-                              $("#toolz").append($toolkit);
-                          })
+                            $(document).ready(function () {
+                                var $toolkit = '<li>' + toolitem + '</li>'
+                                $("#toolz").append($toolkit);
+                            })
 
-                      }
-                        
+                        }
+
                     })
 
                     // Object.keys(userattributes).forEach(key=>{
@@ -71,14 +71,26 @@ sayHello();
 
 
 document.getElementById("deleteaccount").addEventListener("click", function () {
-   
-    var user = firebase.auth().currentUser;
+    firebase.auth().onAuthStateChanged(function (somebody) {
+        if (somebody) {
+            db.collection("users")
+                .doc(somebody.uid)
+                .get()
+                .then(function (doc) {
+                    doc.update({
+                        description: firebase.firestore.FieldValue.delete()
+                    })
 
-    user.delete().then(function() {
-    }).catch(function(error) {
-    });
+                    //             }
 
-})
+                    // user.delete().then(function() {
+                    // }).catch(function(error) {
+                    // });
+
+                })
+        }
+    })
+});
 
 
 
@@ -137,11 +149,11 @@ document.getElementById("deleteaccount").addEventListener("click", function () {
 //                 .then(function (doc) {
 //                     var reviews = doc.data().reviews;
 //                     for (let i = 0; i < reviews.length; i++) {
-        
-        
+
+
 //                         let WrittenReviews = reviews[i].review;
 //                         let rating = reviews[i].rating;
-        
+
 //                         var postedreview = $('<div id="reviews"></div>');
 //                         var cardformat = $('<div class="card"></div>');
 //                         var cardclass = $('<div class="card-header"></div>');
@@ -150,7 +162,7 @@ document.getElementById("deleteaccount").addEventListener("click", function () {
 //                         var blockquote = $('<blockquote class="blockquote mb-0"> </blockquote>');
 //                         var reviewerstatement = $('<p id="reviewer-statement"></p>');
 //                         var reviewername = $('<footer id="reviewer-name" class="blockquote-footer"> Francis Boomer <cite title="Source Title"></cite>');
-        
+
 //                         blockquote.append(reviewerstatement);
 //                         blockquote.append(reviewername);
 //                         cardbody.append(blockquote);
@@ -158,15 +170,15 @@ document.getElementById("deleteaccount").addEventListener("click", function () {
 //                         cardclass.append(cardbody);
 //                         cardformat.append(cardclass);
 //                         postedreview.append(cardformat);
-        
-        
+
+
 //                         postedreview.find('#thisagain').append(StarCreation(rating));
 //                         postedreview.find('#reviewer-statement').text(WrittenReviews);
 //                         $('#reviews').append(postedreview);
 //                     } 
 //                 })
 //         }
-                
+
 //                 })
 //         }
 
