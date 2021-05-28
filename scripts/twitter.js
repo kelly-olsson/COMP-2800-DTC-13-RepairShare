2b8f0d05f302186df735353b6b15a7a0ba28006b
Mahan Esmaeilzadeh <mesmaeilzadeh@my.bcit.ca>
2021-05-28 00:05:45 -0700

    Add citation to twitter.js

--- scripts/twitter.js
+++ scripts/twitter.js
@@ -1,3 +1,6 @@
+/**
+ * I found this code from: https://developer.twitter.com/en/docs/twitter-for-websites/javascript-api/guides/set-up-twitter-for-websites
+ */
 window.twttr = (function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0],
         t = window.twttr || {};
@@ -14,49 +17,3 @@ window.twttr = (function (d, s, id) {
 
     return t;
 }(document, "script", "twitter-wjs"));
-
-(function() {
-    if (window.__twitterIntentHandler) return;
-    var intentRegex = /twitter\.com\/intent\/(\w+)/,
-        windowOptions = 'scrollbars=yes,resizable=yes,toolbar=no,location=yes',
-        width = 550,
-        height = 420,
-        winHeight = screen.height,
-        winWidth = screen.width;
-  
-    function handleIntent(e) {
-      e = e || window.event;
-      var target = e.target || e.srcElement,
-          m, left, top;
-  
-      while (target && target.nodeName.toLowerCase() !== 'a') {
-        target = target.parentNode;
-      }
-  
-      if (target && target.nodeName.toLowerCase() === 'a' && target.href) {
-        m = target.href.match(intentRegex);
-        if (m) {
-          left = Math.round((winWidth / 2) - (width / 2));
-          top = 0;
-  
-          if (winHeight > height) {
-            top = Math.round((winHeight / 2) - (height / 2));
-          }
-  
-          window.open(target.href, 'intent', windowOptions + ',width=' + width +
-                                             ',height=' + height + ',left=' + left + ',top=' + top);
-          e.returnValue = false;
-          e.preventDefault && e.preventDefault();
-        }
-      }
-    }
-  
-    if (document.addEventListener) {
-      document.addEventListener('click', handleIntent, false);
-    } else if (document.attachEvent) {
-      document.attachEvent('onclick', handleIntent);
-    }
-    window.__twitterIntentHandler = true;
-  }());
-
-
